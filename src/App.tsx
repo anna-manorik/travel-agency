5273461a3a4f2dda1aebfb11ed4f72c4c7846a8d
Manoryk Hanna <86349455+anna-manorik@users.noreply.github.com>
2025-04-07 23:06:45 +0300

    added complete functionality to Task component

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,17 @@ import './App.css'
 import Task from './components/Task'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isCompleted, toggleComplete] = useState(false)
   const state = {
     title: 'first task',
     description: 'this is the first task of my app'
   }
 
+
   return (
     <>
       <h1>My App</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Task title={state.title} description={state.description} isCompleted={false} />
+      <Task title={state.title} description={state.description} isCompleted={isCompleted} toggleComplete={toggleComplete} />
     </>
   )
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,13 +2,18 @@ type Props = {
     title: string;
     description: string;
     isCompleted: boolean;
+    toggleComplete: Function;
   };
 
-const Task = ({ title, description, isCompleted}: Props) => {
+const Task = ({ title, description, isCompleted, toggleComplete}: Props) => {
     return (
         <>
             <div>{title}</div>
             <div>{description}</div>
+            <span>Is Task completed? {isCompleted ? '✅' : '❌'}</span>
+            <button onClick={() => toggleComplete((isCompleted: boolean) => isCompleted ? false : true)}>
+                {isCompleted ? 'Open' : 'Done'}
+            </button>
         </>
     )
 }
