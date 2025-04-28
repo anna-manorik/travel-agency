13fa33e30ba9c587e3d1eecca989f04b4146ed85
Manoryk Hanna <86349455+anna-manorik@users.noreply.github.com>
2025-04-28 12:40:04 +0300

    added login functionality

---
+++ src/config/firebase.ts
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBIs0d-Q45crU1nNxAQGN5oY-njqOaSjjI",
+  authDomain: "travel-agency-72839.firebaseapp.com",
+  projectId: "travel-agency-72839",
+  storageBucket: "travel-agency-72839.appspot.com",
+  messagingSenderId: "911016679730",
+  appId: "1:911016679730:web:1d96b607478a3582955f2b"
+};
